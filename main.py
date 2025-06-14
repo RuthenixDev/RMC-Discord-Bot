4510c31a07a9ef09b5aeda454849acc8058a9438
justiamtgm <128042664+justiamtgm@users.noreply.github.com>
2025-06-15 00:11:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from discord.ext import commands
 from dotenv import load_dotenv
 import os
 
-load_dotenv(os.getenv("TOKEN"))
-TOKEN = ""
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
 
 bot = commands.Bot(command_prefix="!rmc", intents=discord.Intents.all())
 
